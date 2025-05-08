33a5c72b7725d680097b8b4a6c5c947a12ca7c52
Jeni Lutfi Fauzi <jenilutfifauzi18@gmail.com>
2025-05-08 10:03:03 +0700

    fix: darkmode

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900 mt-4">
+    <footer className="bg-white dark:bg-black mt-4">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
